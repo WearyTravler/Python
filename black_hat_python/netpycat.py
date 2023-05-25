d8bcff4da04137c8969c49fac5e102d8d2b3d9ec
WearyTravler <limbo_shell@protonmail.com>
2023-05-25 09:41:09 -0500

    created client code

--- black_hat_python/netpycat.py
+++ black_hat_python/netpycat.py
@@ -88,3 +88,45 @@ def main():
             server_loop()
 
 main()
+
+
+def client_sender(buffer):
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+
+            # connect to our target host
+            client.connect((target.port))
+
+            if len(buffer):
+                client.send(buffer)
+
+            while True:
+
+                    # now wait for data back
+                    recv_len = 1 
+                    response = ""
+
+                    while recv_len:
+
+                            data        = client.recv(4096)
+                            recv_len    = len(data)
+                            response   += data 
+
+                            if recv_len < 4096:
+                                    break
+                    print response,
+
+                    # wait for more input
+                    buffer  = raw_input("")
+                    buffer += "\n"
+
+                    # send it off
+                    client.send(buffer)
+    except:
+
+            print("[*] Exception! Exiting.")
+
+            # tear down the connection
+            client.close()
