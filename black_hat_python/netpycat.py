4da0f868ec55cd166d0747d9a4a7ad7aadde1b26
WearyTravler <limbo_shell@protonmail.com>
2023-05-25 09:59:53 -0500

    server_loop()

--- black_hat_python/netpycat.py
+++ black_hat_python/netpycat.py
@@ -130,3 +130,38 @@ def client_sender(buffer):
 
             # tear down the connection
             client.close()
+
+def server_loop():
+        global target
+        
+        # if no target is defined, we listen on all interfaces
+
+        if not len(target):
+            target = "0.0.0.0"
+
+            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server.bind((target,port))
+            server.listen(5)
+
+            while True:
+                    client_socket, addr = server.accept()
+
+                    # spin off a thread to handle our new client
+                    client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+                    client_thread.start()
+
+def run_command(command):
+
+    # trim the newline
+
+    command = command.rstrip()
+
+    # run the command and get the output back
+
+    try:
+            output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=True)
+    except:
+            output = "Failed to execute command.\r\n"
+    
+    # send the output back to the client
+    return output
